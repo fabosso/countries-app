abe75bb00d7f53889fbe65b22516e5dda6d9dbb1
Franco Abosso <franco.abosso@gm2dev.com>
2021-08-24 16:54:59 -0300

    Replace regions array with proposed values

--- src/utils/filterCountryOrRegion.js
+++ src/utils/filterCountryOrRegion.js
@@ -1,14 +1,14 @@
 export const filterCountryOrRegion = ({ countries, word, region }) => {
-    return countries.filter((data, index) => {
-      if (!word && !region) {
-        return data;
-      } else if (
-        (word && data.name.toLowerCase().includes(word.toLowerCase())) ||
-        (region && data.region.toLowerCase() === region.toLowerCase())
-      ) {
-        return data;
-      }
-      return null;
-    });
-  };
-  
+  region = region === "America" ? "Americas" : region;
+  return countries.filter((data, index) => {
+    if (!word && !region) {
+      return data;
+    } else if (
+      (word && data.name.toLowerCase().includes(word.toLowerCase())) ||
+      (region && data.region.toLowerCase() === region.toLowerCase())
+    ) {
+      return data;
+    }
+    return null;
+  });
+};
