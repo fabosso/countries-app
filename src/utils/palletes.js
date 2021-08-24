cf808c55817bbbf55711fa532709a207436b5e4f
Franco Abosso <franco.abosso@gm2dev.com>
2021-08-24 12:05:46 -0300

    bugfix(darkTheme): change hardcoded values

--- src/utils/palletes.js
+++ src/utils/palletes.js
@@ -4,11 +4,13 @@ export const palletes = {
     "--elements-color": "hsl(209, 23%, 22%)",
     "--shade-color": "hsl(207, 26%, 7%)",
     "--text-color": "hsl(0, 0%, 100%)",
+    "--input-color": "hsl(0, 0%, 98%)",
   },
   light: {
     "--bg-color": "hsl(0, 0%, 98%)",
     "--elements-color": "hsl(0, 0%, 100%)",
     "--shade-color": "hsl(0, 0%, 52%)",
     "--text-color": "hsl(200, 15%, 8%)",
+    "--input-color": "hsl(0, 0%, 52%)",
   },
 };
