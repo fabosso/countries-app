8f0b643379da28c4cb6da62b7e6f0e9eabcaa56a
Cristian Saldivia <cristiansaldivia@MacBook-Pro-de-Cristian.local>
2021-08-24 13:20:04 -0300

    add fetch data

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,3 @@
-import dataJSON from "../../data.json";
 import { Search } from "../../components/Search/Search";
 import { useField } from "../../hooks/useField";
 import { FlagGrid } from "../../components/FlagGrid/FlagGrid";
@@ -6,11 +5,20 @@ import { FilterBySelector } from "../../components/FilterBySelector/FilterBySele
 import { useSelect } from "../../hooks/useSelect";
 import styles from "./styles.module.scss";
 import "../../assets/styles/Globals.scss";
-
-const regions = ["Americas", "Asia", "Africa", "Europe", "Oceania"];
+import { regions } from "./Home.constants";
+import { getAll } from "../../services/api";
+import { useFetch } from "../../hooks/useFetch";
+import { useEffect } from "react";
 export const Home = () => {
   const { resetSearchValue, ...search } = useField("text");
   const { resetSelectValue, ...select } = useSelect({ initialState: regions });
+  const { value: countries, doFetch: getCountries } = useFetch({
+    fetch: getAll,
+  });
+  useEffect(() => {
+    getCountries();
+  }, [getCountries]);
+
   return (
     <>
       <div className={styles.container}>
@@ -21,11 +29,17 @@ export const Home = () => {
             resetSearchValue={resetSearchValue}
           />
         </div>
-        <FlagGrid
-          countries={dataJSON}
-          wordSearch={search.value}
-          region={select.selectedValue}
-        />
+        {!countries?.length ? (
+          <div className={styles.spinner_content}>
+          <div className={styles.spinner}></div>
+          </div>
+        ) : (
+          <FlagGrid
+            countries={countries}
+            wordSearch={search.value}
+            region={select.selectedValue}
+          />
+        )}
       </div>
     </>
   );
