84cfb6e5ec569b8cb73d8cc85d1a1f4d9efe8d0e
Franco Abosso <franco.abosso@gm2dev.com>
2021-08-26 16:32:53 -0300

    Small changes to skeleton

--- src/components/CountryCard/CountryCardLoader.js
+++ src/components/CountryCard/CountryCardLoader.js
@@ -1,27 +1,40 @@
 import styles from "./styles.module.scss";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useTheme } from "../../context/themeContext";
+import { palletes } from "../../utils/palletes";
+import { getLocalValue } from "../../utils/localStorage";
+
 export const CountryCardLoader = ({ country }) => {
+  const { theme } = useTheme();
+  const themeLS = getLocalValue("theme");
+  const properties = themeLS ? palletes[themeLS] : palletes[theme];
+
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper__img}>
-        <Skeleton className={styles.country_img} />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.content__text}>
-          <h2>
-            <Skeleton />
-          </h2>
-          <p className={styles.paragraph}>
-            <Skeleton />
-          </p>
-          <p className={styles.paragraph}>
-            <Skeleton />
-          </p>
-          <p className={styles.paragraph}>
-            <Skeleton />
-          </p>
+      <SkeletonTheme
+        color={properties["--skeleton-color"]}
+        highlightColor={properties["--shine-color"]}
+      >
+        <div className={styles.wrapper__img}>
+          <Skeleton className={styles.country_img} height={160} />
         </div>
-      </div>
+        <div className={styles.content}>
+          <div className={styles.content__text}>
+            <h2>
+              <Skeleton />
+            </h2>
+            <p className={styles.paragraph}>
+              <Skeleton />
+            </p>
+            <p className={styles.paragraph}>
+              <Skeleton />
+            </p>
+            <p className={styles.paragraph}>
+              <Skeleton />
+            </p>
+          </div>
+        </div>
+      </SkeletonTheme>
     </div>
   );
 };
