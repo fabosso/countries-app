c81df006393afaaf058ee77c80be9386c068932f
Cristian Saldivia <cristiansaldivia@MacBook-Pro-de-Cristian.local>
2021-08-26 14:47:07 -0300

    add types

--- src/hooks/useSelect.ts
+++ src/hooks/useSelect.ts
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import { InputEvent } from "../types/EventTypes";
 
 type selectParams = {
-  initialState: any[]
-}
+  initialState: any;
+};
+type useSelectType = {
+  values: any;
+  selectedValue: any;
+  onChange: (event: InputEvent) => void;
+  resetSelectValue: () => void;
+};
 
-export const useSelect = ({ initialState = [] }:selectParams) => {
+export const useSelect = ({
+  initialState = [],
+}: selectParams): useSelectType => {
+  
   const values = initialState;
   const [selectedValue, setSelectedValue] = useState<string>("");
-  const onChange = (event:any) => {
+  const onChange = (event: InputEvent): void => {
     setSelectedValue(event.target.value);
   };
-  const resetSelectValue = () => {
+  const resetSelectValue = (): void => {
     setSelectedValue("");
   };
 
