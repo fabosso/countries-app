bffc2ecec44064293420f111a87fbc3ab7d5c269
Franco Abosso <franco.abosso@gm2dev.com>
2021-08-30 12:45:39 -0300

    Add updateLastVisited fcn

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -4,19 +4,24 @@ import {
   getLocalValue,
   setLocalValue,
   removeLocalValue,
+  reformLastVisited,
 } from "../utils/localStorage";
 const globalContext = createContext();
 
 export function GlobalProvider(props) {
-  const themeLocalStorage = getLocalValue("theme");
-  const lastVisitedLocalStorage = getLocalValue("lastVisited");
+  const themeLocalStorage = getLocalValue("theme")
+    ? getLocalValue("theme")
+    : "light";
+  const lastVisitedLocalStorage = getLocalValue("lastVisited")
+    ? getLocalValue("lastVisited")
+    : [];
   const [lastVisited, setLastVisited] = useState(lastVisitedLocalStorage);
   const [theme, setTheme] = useState(themeLocalStorage);
   const [transitions, setTransitions] = useState(false);
   const [borders, setBorders] = useState(null);
 
   useEffect(() => {
-    const properties = theme ? palletes[theme] : palletes["light"];
+    const properties = palletes[theme];
     for (const [key, value] of Object.entries(properties)) {
       document.documentElement.style.setProperty(key, value);
     }
@@ -38,13 +43,22 @@ export function GlobalProvider(props) {
     setLocalValue("theme", _theme);
   };
 
+  const updateLastVisited = (code, history) => {
+    const _lastVisited = reformLastVisited(code);
+    setLastVisited((prev) => [..._lastVisited]);
+    removeLocalValue("lastVisited");
+    setLocalValue("lastVisited", _lastVisited);
+    setBorders(null); // null signalizes a temporal lack of downloaded data, NOT a lack of borders
+    history.push(`/description/${code}`);
+  };
+
   return (
     <globalContext.Provider
       value={{
         darkModeHandler,
         setTransitions,
         lastVisited,
-        setLastVisited,
+        updateLastVisited,
         borders,
         setBorders,
         theme,
