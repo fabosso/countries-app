4725cb2f1ff566d811a772718db4c79cc1196470
Franco Abosso <franco.abosso@gm2dev.com>
2021-09-01 12:09:45 -0300

    Regions Type

--- src/utils/getKeyByValue.ts
+++ src/utils/getKeyByValue.ts
@@ -1,9 +1,8 @@
+import { RegionsType } from "../types/Regions.type";
+
 export const getKeyByValue = (
-  object: any,
+  object: RegionsType,
   value: string
 ): string | undefined => {
   return Object.keys(object).find((key: string) => object[key] === value);
 };
-
-
-
