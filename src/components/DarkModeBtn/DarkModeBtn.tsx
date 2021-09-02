e06389aa2b337bc2fb10c37800e015de2b555333
Franco Abosso <franco.abosso@gm2dev.com>
2021-09-02 16:18:16 -0300

    Display dark / light mode

--- src/components/DarkModeBtn/DarkModeBtn.tsx
+++ src/components/DarkModeBtn/DarkModeBtn.tsx
@@ -1,15 +1,22 @@
 import { useGlobal } from "../../context/globalContext";
 import styles from "./styles.module.scss";
-import { MoonOutline } from "react-ionicons";
+import { MoonOutline, SunnyOutline } from "react-ionicons";
 
 const DarkModeBtn = () => {
-  const { darkModeHandler } = useGlobal();
+  const { darkModeHandler, theme } = useGlobal();
   return (
     <span className={styles.button} onClick={darkModeHandler}>
-      <span>
-        <MoonOutline height="16px" width="16px" color="inherit" />
-        <span>Dark Mode</span>
-      </span>
+      {theme === "light" ? (
+        <span>
+          <MoonOutline height="16px" width="16px" color="inherit" />
+          <span>Dark Mode</span>
+        </span>
+      ) : (
+        <span>
+          <SunnyOutline height="16px" width="16px" color="inherit" />
+          <span>Light Mode</span>
+        </span>
+      )}
     </span>
   );
 };
